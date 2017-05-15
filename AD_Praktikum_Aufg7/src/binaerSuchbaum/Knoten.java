9781949a415ed0679e01a8d94011650b1e2a0151
Johannes <Johannes@Jojo-Desktop>
2017-05-15 21:46:58 +0200

    Doc Kommentare einfügen

--- AD_Praktikum_Aufg7/src/binaerSuchbaum/Knoten.java
+++ AD_Praktikum_Aufg7/src/binaerSuchbaum/Knoten.java
@@ -6,12 +6,20 @@
 
 package binaerSuchbaum;
 
-public interface Knoten<T extends Comparable<T>> {
+/**
+ * Interface für die darstellung der Knoten
+ * 
+ * @author Johannes
+ *
+ * @param <T>
+ */
+public interface Knoten<T extends Comparable<T>>
+{
 	public T getElement();
 
 	public Knoten<T> getKnotenSonLinks();
 
 	public Knoten<T> getKnotenSonRechts();
-	
+
 	public Knoten<T> getKnotenFather();
 }
