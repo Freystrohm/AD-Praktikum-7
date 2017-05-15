a21196b7b6185367a145c81c97d57f4439b2245c
Johannes <Johannes@Jojo-Desktop>
2017-05-15 21:44:06 +0200

    Doc kommentare einfügen

--- AD_Praktikum_Aufg7/src/binaerSuchbaum/BinSuchbaum.java
+++ AD_Praktikum_Aufg7/src/binaerSuchbaum/BinSuchbaum.java
@@ -4,22 +4,51 @@
  */
 package binaerSuchbaum;
 
+/**
+ * Abstracte Klasse, die als Interface für die beiden Implementationen des
+ * Binärbaumes fungiert und die verschiedenen Order Methoden bereitstellt
+ * 
+ * @author Johannes
+ *
+ * @param <T>
+ */
 public abstract class BinSuchbaum<T extends Comparable<T>>
 {
 	private Knoten<T> wurzel;
-	
+
+	/**
+	 * Funktion zum einfügen eines Knotens in den Binärbaum
+	 * 
+	 * @param knoten:
+	 *            einzufügender Knoten
+	 */
 	public abstract void insertKnoten(Knoten<T> knoten);
-	
-	public T[] getInsorder()
+
+	/**
+	 * Funktion für die Array darstellung des baumes nach dem Inorder Prinzip
+	 * 
+	 * @return: Der Baum dargestellt als nach inorder sortiertes Array
+	 */
+	public T[] getInorder()
 	{
 		return null;
 	}
-	
+
+	/**
+	 * Funktion für die Array darstellung des baumes nach dem Preorder Prinzip
+	 * 
+	 * @return: Der Baum dargestellt als nach preorder sortiertes Array
+	 */
 	public T[] getPreorder()
 	{
 		return null;
 	}
-	
+
+	/**
+	 * Funktion für die Array darstellung des baumes nach dem Postorder Prinzip
+	 * 
+	 * @return: Der Baum dargestellt als nach postorder sortiertes Array
+	 */
 	public T[] getPostorder()
 	{
 		return null;
